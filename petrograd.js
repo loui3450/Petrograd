d6d41be515fbb44474ffe34f7d40ab1b5a23e146
loui3450 <loui3450@stud.kea.dk>
2017-02-23 13:23:05 +0100

    Henter JSON-fil og kloner template

--- petrograd.js
+++ petrograd.js
@@ -2,14 +2,25 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+    //læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktliste)
+
     visProdukt();
 }
 
-function visProdukt() {
+function visProduktliste(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
+    console.log(produkt);
     //klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 
     //indsæt data i klon
+
 
     //append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
